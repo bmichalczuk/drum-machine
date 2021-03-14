128ee53c5c1a73612350fc6fb5495cd9ecba32ff
bmichalczuk <barmic90@hotmail.com>
2021-03-14 23:15:02 +0100

    change sound sets to objects, display sound set name on change

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -8,17 +8,17 @@ document.querySelector(".keyboard").addEventListener("click", async function(e) 
   e.stopPropagation();
   const {target: {tagName, id}, target} = e;
     if(tagName === "BUTTON") {
-      const {soundSet} = getState();
-      const {url: sound, id: soundName} = soundSet.find(item => item.keyTrigger === id);
+      const {soundSet: {sounds}} = getState();
+      const {url: sound, id: soundName} = sounds.find(item => item.keyTrigger === id);
       playKeyboardKey(sound, soundName, target);
   }
 });
 
 document.querySelector("html").addEventListener("keydown", async function(e) {
   e.stopPropagation();
-  const {soundSet} = getState();
-  if(soundSet.find(item => item.keyTrigger === e.key.toUpperCase())) {
-      const {url: sound, id: soundName, keyTrigger} = soundSet.find(item => item.keyTrigger === e.key.toUpperCase());
+  const {soundSet: {sounds}} = getState();
+  if(sounds.find(item => item.keyTrigger === e.key.toUpperCase())) {
+      const {url: sound, id: soundName, keyTrigger} = sounds.find(item => item.keyTrigger === e.key.toUpperCase());
       setScreenStatus(soundName);
       const btn = document.getElementById(keyTrigger);
       playKeyboardKey(sound, soundName, btn);
@@ -28,8 +28,8 @@ document.querySelector("html").addEventListener("keydown", async function(e) {
 document.querySelector("#bank").addEventListener("change", function(e) {
   e.stopPropagation();
   const {soundSet: currentSoundSet} = getState();
-  console.log(currentSoundSet);
   soundSetHandler(currentSoundSet);
-  console.log(currentSoundSet.constructor.name);
+  const {soundSet: {name}} = getState();
+  setScreenStatus(name);
 
 });
